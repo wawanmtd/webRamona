18b926aefc5ff2827af3e4430ffeb9c1e188edce
muchtarpr <lahir0112>
2016-11-29 06:46:49 +0700

    waktu ke adhy

--- resources/views/admin/kelolaAdmin.blade.php
+++ resources/views/admin/kelolaAdmin.blade.php
@@ -1,36 +1,6 @@
-<!-- <script src="../bootstrap/adminlte/plugins/fastclick/fastclick.min.js"></script> -->
-<!-- <script src="../bootstrap/adminlte/plugins/slimScroll/jquery.slimscroll.min.js"></script> -->
-<!-- <script src="../bootstrap/adminlte/plugins/datatables/jquery.dataTables.min.js"></script> -->
-<!-- <script src="../bootstrap/adminlte/plugins/datatables/dataTables.bootstrap.min.js"></script> -->
-<!-- <script src="../bootstrap/adminlte/js/demo.js"></script> -->
-<script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
-<link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css">
-<script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js "></script>
 @extends('admin.index')
 
-@section('head')
-<script type="text/javascript">
 
-  $(document).on('click', '#ubahAdminModal_ID', function(){
-  var myBook_ID = $(this).data('book-id');
-  return myBook_ID;
-  // $('.modal-body #edit_personname').val(myBook_ID);
-  // $('#ubah').modal('show');
-});
-
-  // $(document).on('click', '.open-addButtonDialog', function(){
-  // var mybookid = $(this).data('id');
-  // $('.modal-body #bookid').val(mybookid);
-  // $('#addBookDialog').modal('show');
-  // });
-
-  //referensi lain
-//   $('#my_modal').on('show.bs.modal', function(e) {
-//     var bookId = $(e.relatedTarget).data('book-id');
-//     $(e.currentTarget).find('input[name="bookId"]').val(bookId);
-// });
-  </script>
-@stop
 
 @section('content-header')
   Kelola Admin
@@ -50,98 +20,64 @@
     </div>
 
     <div class="box-body">
-      <div id="tableAdmin_wrapper" class="dataTables_wrapper form-inline dt-bootstrap">
-        <div class="row">
+      <table id="tableAdmin" class="table table-bordered table-striped">
+        <thead>
+          <tr role="row">
+            <th class="sorting" tabindex="0" aria-controls="tableAdmin" aria-label="Nama Lengkap: active to sort column ascending">Nama Lengkap</th>
+            <th class="sorting" tabindex="0" aria-controls="tableAdmin" aria-label="Username: active to sort column ascending">Username</th>
+            <th class="sorting" tabindex="0" aria-controls="tableAdmin" aria-label="Role: active to sort column ascending">Role</th>
+            <th class="sorting" tabindex="0" aria-controls="tableAdmin" aria-label="Station: active to sort column ascending">Station</th>
+            <th class="sorting" tabindex="0" aria-controls="tableAdmin" aria-label="Area: active to sort column ascending">Country</th>
+            <th colspan="2">Action</th>
+          </tr>
+        </thead>
 
-        </div>
+        <tbody>
 
+        @foreach ($ShowMember as $showmember)
 
-            <table id="tableAdmin" class="table table-bordered table-striped dataTable display" role="grid">
-              <thead>
-                <tr role="row">
-                  <th class="sorting" tabindex="0" aria-controls="tableAdmin" aria-label="Nama Lengkap: active to sort column ascending">Nama Lengkap</th>
-                  <th class="sorting" tabindex="0" aria-controls="tableAdmin" aria-label="Username: active to sort column ascending">Username</th>
-                  <th class="sorting" tabindex="0" aria-controls="tableAdmin" aria-label="Role: active to sort column ascending">Role</th>
-                  <th class="sorting" tabindex="0" aria-controls="tableAdmin" aria-label="Station: active to sort column ascending">Station</th>
-                  <th class="sorting" tabindex="0" aria-controls="tableAdmin" aria-label="Area: active to sort column ascending">Country</th>
-                  <th colspan="2">Action</th>
-                </tr>
-              </thead>
+          <tr class="odd" role="row">
+            <td >{{$showmember->PersonData->PersonName}}</td>
+            <td >{{$showmember->Username}}</td>
+            <td >{{$showmember->MemberRoleData->NameRole}}</td>
+            <td >{{$showmember->StationData->StationName}}</td>
+            <td ><?php if ($showmember->PersonData->CountryData->CountryName ) echo $showmember->PersonData->CountryData->CountryName ?></td>
+            <td style="width:5%">
+              <button type="button" action="#ubah" data-toggle="modal" data-target="#ubahAdminModal" data-content class="btn btn-info" data-ubahbutton="{{$showmember->Member_ID}}"/>
+                <span class="fa fa-edit"></span></button>
+            </td>
+            <td style="width:5%">
+              <button type="button"  action="#hapus" data-toggle="modal" data-target="#hapusAdminModal" data-book-id='asd' class="btn btn-danger">
+                <span class="fa fa-trash"></span></button>
+            </td>
+          </tr>
 
-              <tbody>
+              @endforeach
+        </tbody>
 
-              @foreach ($ShowMember as $showmember)
-
-                <tr class="odd" role="row">
-                  <td >{{$showmember->PersonData->PersonName}}</td>
-                  <td >{{$showmember->Username}}</td>
-                  <td >{{$showmember->MemberRoleData->NameRole}}</td>
-                  <td >{{$showmember->StationData->StationName}}</td>
-                  <td >{{$showmember->PersonData->CountryData->CountryName}}</td>
-                  <td style="width:5%">
-                    <button type="button" action="#ubah" data-toggle="modal" data-target="#ubahAdminModal" data-content class="btn btn-info" id="ubahAdminModal_ID" data-id='asdasd' href='#ubah'>
-                      <span class="fa fa-edit"></span></button>
-                    </td>
-                    <td style="width:5%">
-                      <button type="button"  action="#hapus" data-toggle="modal" data-target="#hapusAdminModal" data-book-id='asd' class="btn btn-danger">
-                        <span class="fa fa-trash"></span></button>
-                      </td>
-                    </tr>
-
-                    @endforeach
-<!-- 
-                    <tr class="even" role="row">
-                      <td >Muchtar Prawira</td>
-                      <td >muchtarpr</td>
-                      <td >Admin</td>
-                      <td >Batan</td>
-                      <td >Puspiptek</td>
-                      <td>
-                        <button type="button" action="#ubah" data-toggle="modal" data-target="#ubahAdminModal" class="btn btn-info">
-                          <span class="fa fa-edit"></span></button>
-                        </td>
-                        <td>
-                          <button type="button" action="#hapus" data-toggle="modal" data-target="#hapusAdminModal" class="btn btn-danger">
-                            <span class="fa fa-trash"></span></button>
-                          </td>
-                        </tr>
-
-                        <tr class="odd" role="row">
-                          <td class="">Gerald Viko Ananda</td>
-                          <td class="">seishiro</td>
-                          <td class="">Manajerial</td>
-                          <td class="">Batan</td>
-                          <td class="">Puspiptek</td>
-                          <td>
-                            <button type="button" action="#ubah" data-toggle="modal" data-target="#ubahAdminModal" class="btn btn-info">
-                              <span class="fa fa-edit"></span></button>
-                            </td>
-                            <td>
-                              <button type="button" action="#hapus" data-toggle="modal" data-target="#hapusAdminModal" class="btn btn-danger">
-                                <span class="fa fa-trash"></span></button>
-                              </td>
-                            </tr> -->
-                          </tbody>
-
-                          <tfoot>
-                            <tr>
-                              <th>Nama Lengkap</th>
-                              <th>Username</th>
-                              <th>Role</th>
-                              <th>Station</th>
-                              <th>Country</th>
-                              <th colspan="2">Action</th>
-                            </tr>
-                          </tfoot>
-                        </table>
-          </div>
-        </div>
-      </div>
-
+        <tfoot>
+          <tr>
+            <th>Nama Lengkap</th>
+            <th>Username</th>
+            <th>Role</th>
+            <th>Station</th>
+            <th>Country</th>
+            <th colspan="2">Action</th>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
     </div>
   </div>
+</div>    
 <!-- end Table Data Admin -->
+<script type="text/javascript">
+//   $(document).ready(function() {
+//     $('#tableAdmin').dataTable();
+// });
 
+
+</script>
 <!-- modal Hapus Admin -->
   <div class="modal fade" id="hapusAdminModal" tab-index="-1" role="dialog" aria-labeledby="hapusAdminModalLabel">
     <div class="modal-dialog modal-sm" role="alertdialog">
@@ -252,6 +188,9 @@
 <!-- end modal Tambah Admin -->
 
 <!-- modal Ubah Admin -->
+
+
+
   <div class="modal fade" id="ubahAdminModal" tabindex="0" role="dialog" aria-labelledby="ubahAdminModalLabel">
     <div class="modal-dialog modal-lg" role="document">
       <div class="modal-content">
@@ -266,7 +205,7 @@
               
               <div class="form-group">
                 <label for="FullName">Full Name</label>
-                <input type="text" name="PersonName" class="form-control" placeholder="Full Name" required>
+                <input type="text" value="" data-fullname="" name="PersonName" class="form-control personname" placeholder="Full Name" required>
               </div>
 
               <div class="form-group">
@@ -343,11 +282,19 @@
 <!-- end Ubah Admin -->
 
 
-
 <script>
-$(document).ready(function() {
-    $('#tableAdmin').dataTable();
-});
+$(document).ready(function(){
+  $('#ubahAdminModal').on('show.bs.modal', function(event){
+    var button = $(event.relatedTarget);
+    var recipient = button.data('ubahbutton');
+    var row = button.closest("tr");
+    var person = row.find("td:nth-child(1)").text();
 
+    var modal = $(this);
+    modal.find('.modal-body .personname').val(person);
+  });
+
+});
 </script>
+
 @stop
